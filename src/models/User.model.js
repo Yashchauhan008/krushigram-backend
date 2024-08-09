843ca7a39577ad6dc2696c7e880b4c865e0a0901
yash <yashchauhan1775@gmail.com>
2024-08-10 01:06:42 +0530

    initial commmit

---
+++ src/models/User.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true },
+  },
+  isSeller: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,  // Automatically adds `createdAt` and `updatedAt` fields
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ src/server.js
@@ -0,0 +1,8 @@
+
+const { app } = require('./app.js');
+
+const port=process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log("Server is running at port :", port);
+})
