ec2dcb7e751f7d7b0de3ad35f5c0396d053c717c
yash <yashchauhan1775@gmail.com>
2024-08-10 18:20:34 +0530

    user model working

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,3 +1,46 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     username: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//       lowercase: true,
+//     },
+//     phoneNumber: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//     },
+//     address: {
+//       street: { type: String },
+//       city: { type: String },
+//       state: { type: String },
+//       pinCode: { type: String },
+//     },
+//     isSeller: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   {
+//     timestamps: true, // Automatically adds `createdAt` and `updatedAt` fields
+//   }
+// );
+
+
+// const User = mongoose.model("User", userSchema);
+
+// module.exports = User;
+
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
@@ -35,7 +78,6 @@ const userSchema = new mongoose.Schema(
     timestamps: true, // Automatically adds `createdAt` and `updatedAt` fields
   }
 );
-
 
 const User = mongoose.model("User", userSchema);
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,3 +1,13 @@
+// const { Router } = require("express");
+// const { signup, updateUser } = require("../controllers/user.controller");
+
+// const router = Router();
+
+// router.route("/signup").post(signup);
+// router.route("/update").patch(updateUser);
+
+// module.exports = router;
+
 const { Router } = require("express");
 const { signup, updateUser } = require("../controllers/user.controller");
 
